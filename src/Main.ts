f841c08183e6461abad90501c467605c19d5b2c3
unknown <Bunyoung.Hean@pnc.lan>
2025-05-31 13:22:53 +0700

    Added class Notification

--- src/Main.ts
+++ src/Main.ts
@@ -1,4 +1,5 @@
 import { User } from "./User";
+import { Team } from "./Team";
 
 
 // User Class
@@ -7,3 +8,10 @@ console.log(`User Name: ${chhorrina.getName()} has logged in.`);
 console.log(`User Name: ${chhorrina.logout()} has logged out.`);
 console.log(`User Name: ${chhorrina.getName()} has added a new project.`);
 chhorrina.addProject("New Project");
+
+//Team Class
+let team = new Team("Development", 1);
+team.addMember(chhorrina);
+console.log(`Team Name: ${team.getTeamName()}`);
+console.log(`Project ID: ${team.getProjectId()}`);
+console.log(`Team Members: ${team.getMembers().map(member => member.getName()).join(", ")}`);
